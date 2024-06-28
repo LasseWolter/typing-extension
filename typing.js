f2616ef9d01ae5678baee14d76beaf737c8248ac
Lasse Wolter <wolter.lasse@gmail.com>
2024-06-28 12:31:44 +0100

    Reorder code for clarity

--- typing.js
+++ typing.js
@@ -2,12 +2,63 @@
 // Will load offlineFile (specified below) from disk instead of trying to fetch it from the internet
 const offline = true;
 const offlineFile = "sample/caption.xml";
-
 // ================== CONFIGS END ==================
 
-var videoContainer = document.querySelector(".html5-video-container");
-var placeholderText = "Enable captions to see text here...";
-var captions;
+const placeholderText = "Enable captions to see text here...";
+let captions;
+
+let textSoFarArr = [];
+
+addCaptionBox();
+if (offline) {
+  console.log("OFFLINE MODE");
+  fetchCaptionsFromFile();
+} else {
+  fectchCaptions();
+}
+
+const speed = 10;
+const badge = document.getElementById("badgyBadge");
+
+let counter = 0;
+let idx = 0;
+let text = "";
+
+var intervalID = setInterval(function() {
+  counter++;
+  if (!captions || idx == captions.length - 1) {
+    console.log("Reached end of the file");
+    clearInterval(intervalID);
+  }
+  if (counter % speed === 0) {
+    text = text + "\n" + captions[idx];
+    idx++;
+  }
+
+  if (idx === 0) {
+    badge.innerText = placeholderText;
+  } else {
+    badge.innerText = text;
+  }
+}, 100); // 1000 milliseconds = 1 second
+
+function addCaptionBox() {
+  const badge = document.createElement("div");
+  // Use the same styling as the publish information in an article's header
+  badge.id = "badgyBadge";
+  badge.textContent = placeholderText;
+  badge.style.background = "#d794d7";
+  badge.style.fontSize = "18px";
+  badge.style.margin = "10px";
+  badge.style.width = "100vw";
+  badge.style.padding = "20px";
+  badge.style.position = "absolute";
+  badge.style.top = "10px";
+  badge.style.zIndex = 99999999; // needs to higher than all other page elements
+  badge.style.borderRadius = "5px";
+
+  document.body.insertBefore(badge, document.body.firstChild);
+}
 
 function decodeUnicodeEscapeSequence(str) {
   return str
@@ -70,28 +121,6 @@ function parseXML(xmlStr) {
   return textContents;
 }
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (videoContainer) {
-  const badge = document.createElement("div");
-  // Use the same styling as the publish information in an article's header
-  badge.id = "badgyBadge";
-  badge.textContent = placeholderText;
-  badge.style.background = "#d794d7";
-  badge.style.fontSize = "18px";
-  badge.style.margin = "10px";
-  badge.style.width = "100vw";
-  badge.style.padding = "20px";
-  badge.style.position = "absolute";
-  badge.style.top = "10px";
-  badge.style.zIndex = 99999999;
-  badge.style.borderRadius = "5px";
-
-  document.body.insertBefore(badge, document.body.firstChild);
-}
-
-var badge = document.getElementById("badgyBadge");
-
-var textSoFarArr = [];
 function textSoFar() {
   // Code to update the UI goes here
   let segments = document.querySelectorAll(".ytp-caption-segment");
@@ -113,34 +142,3 @@ function textSoFar() {
 
   return textSoFarArr.slice(-idx).join("\n");
 }
-
-var speed = 10;
-
-let counter = 0;
-let idx = 0;
-let text = "";
-
-if (offline) {
-  console.log("OFFLINE MODE");
-  fetchCaptionsFromFile();
-} else {
-  fectchCaptions();
-}
-
-var intervalID = setInterval(function() {
-  counter++;
-  if (!captions || idx == captions.length - 1) {
-    console.log("Reached end of the file");
-    clearInterval(intervalID);
-  }
-  if (counter % speed === 0) {
-    text = text + "\n" + captions[idx];
-    idx++;
-  }
-
-  if (idx === 0) {
-    badge.innerText = placeholderText;
-  } else {
-    badge.innerText = text;
-  }
-}, 100); // 1000 milliseconds = 1 second
